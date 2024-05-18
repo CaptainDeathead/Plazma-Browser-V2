b181d6644bb2f9ae29b7dd546a5d06f158ee3240
CaptainDeathead <unstableplazma@gmail.com>
2024-05-18 18:44:29 +0800

    Bug fixes + cleaning

--- Engine/renderer.py
+++ Engine/renderer.py
@@ -1,6 +1,5 @@
 import pygame as pg
 import pygame_gui as pgu
-from Ui.elements import H1
 from Engine.DOM.document import Document
 from Engine.html_parser import HTMLParser
 from Engine.STR.renderer import StyledText
@@ -9,7 +8,7 @@ from config import WIN_WIDTH, WIN_HEIGHT
 class Renderer:
     def __init__(self, manager: pgu.UIManager):
         self.manager: pgu.UIManager = manager
-        self.styled_text: StyledText = StyledText("\n", WIN_WIDTH, WIN_HEIGHT, (0, 0, 0), (255, 255, 255), "Calibri", 16, (2, 20, 2, 20))
+        self.styled_text: StyledText = StyledText("\n", WIN_WIDTH, WIN_HEIGHT, (0, 0, 0), (255, 255, 255), "Arial", 16, (2, 20, 2, 20))
         self.html_parser: HTMLParser = HTMLParser(self.manager, self.styled_text)
 
     def loadHTML(self, html: str) -> Document | None:
