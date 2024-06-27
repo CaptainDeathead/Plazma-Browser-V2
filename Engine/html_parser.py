45e0673d6896aada16fa84b9a27d7c70f893c68b
CaptainDeathead <unstableplazma@gmail.com>
2024-06-27 21:34:59 +0800

    Moving  to a non recursive solution

--- Engine/html_parser.py
+++ Engine/html_parser.py
@@ -10,7 +10,7 @@ from pygame_gui import UIManager
 from Ui.elements import UI_TEXT_TAG_SIZES, TEXT_TAGS, USE_TEXTBOX_TAGS, INLINE_ELEMENTS
 from re import finditer
 from copy import deepcopy
-from typing import Dict, List
+from typing import Dict, List, Tuple
 from Engine.Utils.utils import remove_units
 from config import SHOW_PRIMARY_SURFACE_CONTAINERS, BASE_TITLE
 
@@ -47,7 +47,116 @@ class HTMLParser:
     
     def populateElementTree(self, tag: element.Tag) -> None:
 
-        ...
+        nodeStack: List[Tuple[element.Tag, Element, int, int]] = [(tag, self.document.html_element, 0, 0)]
+
+        while len(nodeStack) > 0:
+            if self.stop_loading: return
+
+            tag, parent_element, inline_elements, depth = nodeStack.pop()
+
+            children_to_sort: List[Tuple[element.Tag, Element, int, int]] = []
+
+            for child_tag in tag.children:
+                if isinstance(child_tag, element.Tag):
+                    if child_tag.name in INLINE_ELEMENTS: inline_elements += 1
+                    else: inline_elements = 0
+
+                    child_tag.attrs["tag"] = child_tag.name
+                    child_tag.attrs["text"] = child_tag.text.replace('\n', '')
+                    child_tag.attrs["html"] = str(child_tag).replace('\n', '')
+
+                    # head tags
+                    if child_tag.name == "title": set_caption(BASE_TITLE + child_tag.attrs['text'])
+
+                    text_rect: pg.Rect = parent_element.rect
+                    text_rect_unused: pg.Rect = parent_element.rect_unused
+
+                    # inherit parent styles
+                    tag_styles: Dict[str, any] = parent_element.styles.copy()
+
+                    # deepcopy if it contains nestled things like lists or whatever (points to the same reference)
+                    if tag_styles is parent_element.styles: tag_styles: Dict[str, any] = deepcopy(parent_element.styles)
+
+                    # add styles for text info when sizing fonts
+                    tag_styles["text-tag-size"] = UI_TEXT_TAG_SIZES.get(child_tag.name, 16)
+                    tag_styles["parent-tag-size"] = parent_element.styles.get("text-tag-size", 16)
+
+                    if child_tag.name == 'i': tag_styles["italic"] = True
+                    elif child_tag.name == 'b' or child_tag.name == 'strong': tag_styles["bold"] = True
+                    elif child_tag.name == 'u': tag_styles["underline"] = True
+                    elif child_tag.name == 'br':
+                        if parent_element is not None and type(parent_element.rect) == pg.Rect:
+                            new_line_height: int = feed_line(20, self.curr_y, tag_styles["font-size"], 16, 2)[1] - self.curr_y
+
+                            parent_element.rect.height += new_line_height
+                            self.curr_y += new_line_height
+                        else:
+                            self.curr_y += tag_styles["font-size"]
+
+                    # ---------- PRE-PARSING ----------
+
+                    child_tag_html: str = child_tag.attrs["html"]
+
+                    if child_tag.name in TEXT_TAGS and child_tag.text != "":
+                        if '<browser_text>' not in child_tag.attrs["html"]:
+                            open_tags = [tag.start() for tag in finditer('<', child_tag_html)]
+                            close_tags = [tag.start() for tag in finditer('>', child_tag_html)]
+
+                            tag_offset: int = 0
+            
+                            for i in range(0, len(close_tags)-1, 1):
+                                if child_tag_html[close_tags[i]+tag_offset+1] != '<':
+                                    # <browser_text> has a length of 14
+                                    child_tag.attrs["html"] = "{}{}{}".format(child_tag_html[:close_tags[i]+tag_offset+1], "<browser_text>", child_tag_html[close_tags[i]+tag_offset+1:])
+                                    child_tag_html = child_tag.attrs["html"]
+                                    tag_offset += 14
+                                    child_tag.attrs["html"] = "{}{}{}".format(child_tag_html[:open_tags[i+1]+tag_offset], "</browser_text>", child_tag_html[open_tags[i+1]+tag_offset:])
+                                    child_tag_html = child_tag.attrs["html"]
+                                    tag_offset += 15
+
+                            # reload child tag with formatted html_text
+                            child_tag = BeautifulSoup(child_tag_html, 'html.parser')
+
+                            child_tag.attrs["tag"] = child_tag.name
+                            child_tag.attrs["text"] = child_tag.text.replace('\n', '')
+                            child_tag.attrs["html"] = str(child_tag).replace('\n', '')
+
+                            # feed line because its a block element
+                            if child_tag.name in INLINE_ELEMENTS:
+                                container_element = self.find_closest_container(parent_element)
+                                if container_element is not None: container_element.rect.height += self.styled_text.renderStyledText('\n', )[0].height
+                            
+                            else:
+                                new_line_height: int = feed_line(20, self.curr_y, tag_styles["font-size"], 16, 2)[1] - self.curr_y
+                            
+                                text_rect.height += new_line_height
+                                self.curr_y += new_line_height
+                        else:
+                            tag_styles['font-size'] = UI_TEXT_TAG_SIZES.get(child_tag.name, 16)
+
+                    # ---------- PARSING ----------
+
+                    if "style" in child_tag.attrs:
+                        self.css_parser.load_css_from_str_no_selector(tag_styles, child_tag.attrs["style"], child_tag.name, parent_element.styles["font-size"])
+
+                    element_width: int = int(remove_units(str(child_tag.attrs.get("width", tag_styles.get("width", 0))),
+                                                        parent_element.rect.width, parent_element.rect.width,
+                                                        self.container_width, self.container_height))
+                    
+                    element_height: int = int(remove_units(str(child_tag.attrs.get("height", tag_styles.get("height", 0))),
+                                                        parent_element.rect.height, parent_element.rect.height,
+                                                        self.container_width, self.container_height))
+                    
+                    new_element: Element = Element(child_tag.name, child_tag.attrs, tag_styles, element_width,
+                                                element_height, parent_element, inline_elements, depth)
+
+                    new_element.resize_family_rects(parent_element)
+
+                    parent_element.children.append(new_element)
+
+                    children_to_sort.append((child_tag, parent_element.children[-1], inline_elements, depth + 1))
+
+            nodeStack.extend(reversed(children_to_sort))
 
     def recurse_tag_children(self, tag: element.Tag, parent_element: Element, inline_elements: int = 0, depth: int = 0) -> None:
         
@@ -219,7 +328,8 @@ class HTMLParser:
 
         self.document.html_element.styles['font-size'] = 16
 
-        self.recurse_tag_children(first_elem, self.document.html_element)
+        #self.recurse_tag_children(first_elem, self.document.html_element)
+        self.populateElementTree(first_elem)
         if self.stop_loading: return None
 
         logging.debug("Finished parsing html!")
